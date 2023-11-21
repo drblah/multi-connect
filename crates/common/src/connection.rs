a6663fdf5a7192c3e76a5688ba0244d33c40fc1c
Sebastian <theblahblah2001@gmail.com>
2023-11-21 15:47:02 +0100

    common/connection: Run transmit in a thread to prevent blocking

--- crates/common/src/connection.rs
+++ crates/common/src/connection.rs
@@ -1,9 +1,14 @@
 use std::net::SocketAddr;
+use std::thread;
+use std::thread::JoinHandle;
 use std::time::Duration;
 use smol::lock::Mutex;
 use smol::net::UdpSocket;
 use smol::stream::StreamExt;
 use anyhow::Result;
+use log::warn;
+use smol::{Executor, future};
+use smol::channel::{Sender, TrySendError};
 
 #[derive(Debug, PartialEq)]
 pub enum ConnectionState {
@@ -15,6 +20,8 @@ pub enum ConnectionState {
 #[derive(Debug)]
 pub struct Connection {
     socket: UdpSocket,
+    sender_channel: Sender<Vec<u8>>,
+    sender_thread: JoinHandle<()>,
 
     // TODO: Expose this connection timeout as a user configuration
     connection_timeout: Mutex<smol::Timer>,
@@ -24,8 +31,29 @@ pub struct Connection {
 
 impl Connection {
     pub fn new(socket: UdpSocket) -> Connection {
+
+        let (sender, receiver) = smol::channel::bounded::<Vec<u8>>(100);
+
+        let sender_socket = socket.clone();
+        let sender_thread = thread::spawn(move || {
+            let ex = Executor::new();
+
+            ex.spawn(async {
+
+                loop {
+                    let bytes_to_send = receiver.recv().await.unwrap();
+                    sender_socket.send(bytes_to_send.as_slice()).await.unwrap();
+                }
+            }).detach();
+
+            future::block_on(ex.run(future::pending::<()>()));
+        });
+
+
         Connection {
             socket,
+            sender_channel: sender,
+            sender_thread,
             connection_timeout: Mutex::new(smol::Timer::after(Duration::from_secs(10))),
             state: ConnectionState::Startup,
             buffer: Mutex::new([0; 65535]),
@@ -45,7 +73,24 @@ impl Connection {
     }
 
     pub async fn write(&self, packet: Vec<u8>) -> std::io::Result<usize> {
-        self.socket.send(&packet).await
+        //self.socket.send(&packet).await
+        let packet_length = packet.len();
+        match self.sender_channel.try_send(packet) {
+            Ok(()) => {
+                Ok(packet_length)
+            }
+            Err(e) => {
+                match e {
+                    TrySendError::Full(_) => {
+                        warn!("Send channel is full! Dropping packets on device: {}", self.socket.local_addr().unwrap());
+                        Ok(0)
+                    }
+                    TrySendError::Closed(_) => {
+                        panic!("Sender channel closed!")
+                    }
+                }
+            }
+        }
     }
 
     pub async fn await_connection_timeout(&self) -> SocketAddr {
