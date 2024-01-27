0f8bf520e758e2db15666e74361f41868bbdfc92
drblah <theblahblah2001@gmail.com>
2024-01-27 14:47:00 +0100

    Connection: Cache peer_addr
    Turns out socket.peer_addr() makes a syscall and it is super expensive. Therefore we now cache the address

--- crates/common/src/connection.rs
+++ crates/common/src/connection.rs
@@ -24,21 +24,23 @@ pub struct Connection {
     connection_timeout: Mutex<smol::Timer>,
     pub state: ConnectionState,
     buffer: Mutex<[u8; 65535]>,
+    peer_addr: SocketAddr
 }
 
 impl Connection {
     pub fn new(socket: UdpSocket, interface_name: Option<&str>) -> Connection {
-        let destination_ip = socket.peer_addr().unwrap().ip();
+        let destination_socket_addr = socket.peer_addr().unwrap();
         let std_socket = unsafe { std::net::UdpSocket::from_raw_fd(socket.clone().as_raw_fd()) };
         if let Some(interface_name) = interface_name {
             let interface_name = interface_name.to_string();
             Connection {
                 socket,
                 std_socket,
-                name_address_touple: Some((interface_name, destination_ip)),
+                name_address_touple: Some((interface_name, destination_socket_addr.ip())),
                 connection_timeout: Mutex::new(smol::Timer::after(Duration::from_secs(10))),
                 state: ConnectionState::Startup,
                 buffer: Mutex::new([0; 65535]),
+                peer_addr: destination_socket_addr
             }
         } else {
             Connection {
@@ -48,6 +50,7 @@ impl Connection {
                 connection_timeout: Mutex::new(smol::Timer::after(Duration::from_secs(10))),
                 state: ConnectionState::Startup,
                 buffer: Mutex::new([0; 65535]),
+                peer_addr: destination_socket_addr
             }
         }
     }
@@ -61,7 +64,7 @@ impl Connection {
         let mut buffer_lock = self.buffer.lock().await;
         let message_length = self.socket.recv(buffer_lock.as_mut_slice()).await?;
 
-        Ok((buffer_lock[..message_length].to_vec(), self.socket.peer_addr().unwrap()))
+        Ok((buffer_lock[..message_length].to_vec(), self.peer_addr))
     }
 
     pub async fn write(&self, packet: Vec<u8>) -> std::io::Result<usize> {
@@ -85,7 +88,7 @@ impl Connection {
         let mut deadline_lock = self.connection_timeout.lock().await;
 
         deadline_lock.next().await;
-        self.socket.peer_addr().unwrap()
+        self.peer_addr
     }
 
     pub fn get_name_address_touple(& self) -> Option<(String, IpAddr)> {
