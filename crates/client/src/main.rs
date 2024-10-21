19fa230150e21291c96ddf8a6180008005d22c6e
drblah <theblahblah2001@gmail.com>
2024-10-21 14:09:45 +0200

    Tokiofy: Switch executor to tokio current_thread

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -30,8 +30,8 @@ enum Events {
     DuplicationMessage((usize, SocketAddr))
 }
 
-
-fn main() {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     env_logger::init();
 
     let args = Args::parse();
@@ -40,201 +40,199 @@ fn main() {
         serde_json::from_str(std::fs::read_to_string(args.config).unwrap().as_str()).unwrap();
 
     info!("Using config: {:?}", settings);
-    tokio_uring::start(async {
-        // Command socket for selective duplication
-        let duplication_socket = tokio::net::UdpSocket::bind("0.0.0.0:1234").await.unwrap();
-        let mut duplication_message_buffer = [0u8; 1500];
+    // Command socket for selective duplication
+    let duplication_socket = tokio::net::UdpSocket::bind("0.0.0.0:1234").await.unwrap();
+    let mut duplication_message_buffer = [0u8; 1500];
 
-        let mut interface_logger = None;
-        let mut packet_sorter_logger = None;
+    let mut interface_logger = None;
+    let mut packet_sorter_logger = None;
 
-        if let Some(interface_logger_settings) = settings.interface_logger {
-            interface_logger = Some(InterfaceLogger::new(interface_logger_settings.log_path.clone()).await)
+    if let Some(interface_logger_settings) = settings.interface_logger {
+        interface_logger = Some(InterfaceLogger::new(interface_logger_settings.log_path.clone()).await)
+    }
+
+    if let Some(packet_sorter_logger_settings) = settings.packet_sorter_logger {
+        packet_sorter_logger = Some(PacketSorterLogger::new(packet_sorter_logger_settings.log_path.clone()).await)
+    }
+
+    let client_tun_ip = settings.tunnel_config.tunnel_device_address; //"10.12.0.5".parse().unwrap();
+
+    let client_tun_ipv4 = match client_tun_ip {
+        IpAddr::V4(ipv4) => ipv4,
+        IpAddr::V6(ipv6) => {
+            panic!("Tun address is not an IPv4 address: {}", ipv6)
         }
+    };
 
-        if let Some(packet_sorter_logger_settings) = settings.packet_sorter_logger {
-            packet_sorter_logger = Some(PacketSorterLogger::new(packet_sorter_logger_settings.log_path.clone()).await)
-        }
+    let tun = TunBuilder::new()
+        .name("")
+        .tap(false)
+        .packet_info(false)
+        .mtu(settings.tunnel_config.mtu)
+        .up()
+        .address(client_tun_ipv4.clone())
+        .broadcast(Ipv4Addr::BROADCAST)
+        .netmask(settings.tunnel_config.netmask.clone())
+        .try_build()
+        .unwrap();
 
-        let client_tun_ip = settings.tunnel_config.tunnel_device_address; //"10.12.0.5".parse().unwrap();
-
-        let client_tun_ipv4 = match client_tun_ip {
-            IpAddr::V4(ipv4) => ipv4,
-            IpAddr::V6(ipv6) => {
-                panic!("Tun address is not an IPv4 address: {}", ipv6)
-            }
-        };
-
-        let tun = TunBuilder::new()
-            .name("")
-            .tap(false)
-            .packet_info(false)
-            .mtu(settings.tunnel_config.mtu)
-            .up()
-            .address(client_tun_ipv4.clone())
-            .broadcast(Ipv4Addr::BROADCAST)
-            .netmask(settings.tunnel_config.netmask.clone())
-            .try_build()
-            .unwrap();
-
-        // TODO: Can we handle this in a more generic way?
-        let client_socket_address = settings.interfaces.first().unwrap().bind_address; //"172.16.200.2:0".parse().unwrap();
+    // TODO: Can we handle this in a more generic way?
+    let client_socket_address = settings.interfaces.first().unwrap().bind_address; //"172.16.200.2:0".parse().unwrap();
 
 
-        let server_endpoint_id = settings.server_id;
+    let server_endpoint_id = settings.server_id;
 
-        let client_id = settings.peer_id;
+    let client_id = settings.peer_id;
 
-        let mut tun_buffer = [0u8; 65535];
+    let mut tun_buffer = [0u8; 65535];
 
-        let mut static_routes = Vec::new();
-        for route in &settings.static_routes {
-            static_routes.push(
-                Route {
-                    address: route.address,
-                    subnet_mask: route.subnet_mask,
-                }
-            )
-        }
-
+    let mut static_routes = Vec::new();
+    for route in &settings.static_routes {
         static_routes.push(
-            common::router::Route {
-                address: client_tun_ipv4,
-                subnet_mask: settings.tunnel_config.netmask, }
-        );
+            Route {
+                address: route.address,
+                subnet_mask: route.subnet_mask,
+            }
+        )
+    }
 
-        let static_routes = Some(static_routes);
+    static_routes.push(
+        common::router::Route {
+            address: client_tun_ipv4,
+            subnet_mask: settings.tunnel_config.netmask, }
+    );
 
-        let mut connection_manager = connection_manager::ConnectionManager::new(client_socket_address, client_id, client_tun_ip, static_routes, settings.connection_timeout, settings.packet_sorter_deadline);
+    let static_routes = Some(static_routes);
 
-        let mut keepalive_timer = tokio::time::interval(Duration::from_secs(1));
+    let mut connection_manager = connection_manager::ConnectionManager::new(client_socket_address, client_id, client_tun_ip, static_routes, settings.connection_timeout, settings.packet_sorter_deadline);
 
-        let mut connection_info = Vec::new();
+    let mut keepalive_timer = tokio::time::interval(Duration::from_secs(1));
 
-        for interface_config in &settings.interfaces {
-            connection_info.push(ConnectionInfo {
-                interface_name: interface_config.interface_name.clone(),
-                local_address: interface_config.bind_address,
-                destination_address: interface_config.server_address,
-                destination_endpoint_id: server_endpoint_id
-            });
+    let mut connection_info = Vec::new();
 
-            connection_manager.create_new_connection(
-                interface_config.interface_name.clone(),
-                interface_config.bind_address,
-                interface_config.server_address,
-                server_endpoint_id
-            ).await.unwrap()
-        }
+    for interface_config in &settings.interfaces {
+        connection_info.push(ConnectionInfo {
+            interface_name: interface_config.interface_name.clone(),
+            local_address: interface_config.bind_address,
+            destination_address: interface_config.server_address,
+            destination_endpoint_id: server_endpoint_id
+        });
 
-        loop {
-            if connection_manager.has_endpoints() {
-                let wrapped_endpoints =
-                    async { Events::NewEstablishedMessage(connection_manager.await_incoming().await) };
-                let wrapped_connection_timeout =
-                    async { Events::ConnectionTimeout(connection_manager.await_timeout().await) };
-                let wrapped_packet_sorter = async {
-                    Events::PacketSorter(connection_manager.await_packet_sorters().await)
-                };
-                let wrapped_tunnel_device = async {
-                    Events::TunnelPacket(tun.recv(&mut tun_buffer).await)
-                };
-                let wrapped_keepalive_timer = async {
-                    Events::SendKeepalive( keepalive_timer.tick().await )
-                };
-                let wrapped_new_sorted_packet = async {
-                    Events::NewSortedPacket(connection_manager.await_endpoint_sorted_packets().await)
-                };
-                let wrapped_duplication_socket = async {
-                    Events::DuplicationMessage(duplication_socket.recv_from(&mut duplication_message_buffer).await.unwrap())
-                };
+        connection_manager.create_new_connection(
+            interface_config.interface_name.clone(),
+            interface_config.bind_address,
+            interface_config.server_address,
+            server_endpoint_id
+        ).await.unwrap()
+    }
 
-                match wrapped_keepalive_timer
-                    .or(
-                        wrapped_connection_timeout
-                        .race(wrapped_packet_sorter)
-                        .race(wrapped_tunnel_device)
-                        .race(wrapped_endpoints)
-                        .race(wrapped_new_sorted_packet)
-                        .race(wrapped_duplication_socket)
-                    )
-                    .await
-                {
-                    Events::NewEstablishedMessage(result) => match result {
-                        Ok(read_info) => {
-                            //info!("Endpoint: {}, produced message: {:?}", endpointid, message);
-                            connection_manager.handle_established_message(
-                                read_info,
-                                &mut interface_logger).await;
+    loop {
+        if connection_manager.has_endpoints() {
+            let wrapped_endpoints =
+                async { Events::NewEstablishedMessage(connection_manager.await_incoming().await) };
+            let wrapped_connection_timeout =
+                async { Events::ConnectionTimeout(connection_manager.await_timeout().await) };
+            let wrapped_packet_sorter = async {
+                Events::PacketSorter(connection_manager.await_packet_sorters().await)
+            };
+            let wrapped_tunnel_device = async {
+                Events::TunnelPacket(tun.recv(&mut tun_buffer).await)
+            };
+            let wrapped_keepalive_timer = async {
+                Events::SendKeepalive( keepalive_timer.tick().await )
+            };
+            let wrapped_new_sorted_packet = async {
+                Events::NewSortedPacket(connection_manager.await_endpoint_sorted_packets().await)
+            };
+            let wrapped_duplication_socket = async {
+                Events::DuplicationMessage(duplication_socket.recv_from(&mut duplication_message_buffer).await.unwrap())
+            };
 
-                        }
-                        Err(e) => {
-                            error!("Encountered error: {}", e.to_string())
-                        }
-                    },
-                    Events::ConnectionTimeout((endpoint, interface_name, socket)) => {
-                        connection_manager.remove_connection(endpoint, interface_name, socket)
-                    }
-                    Events::PacketSorter(endpoint_id) => {
-                        connection_manager.handle_packet_sorter_deadline(endpoint_id).await;
-                    }
-                    Events::TunnelPacket(maybe_packet) => {
-                        match maybe_packet {
-                            Ok(packet_length) => {
-                                connection_manager.handle_packet_from_tun(&tun_buffer[..packet_length]).await;
-                            }
-                            Err(e) => error!("Error while reading from tun device: {}", e.to_string())
-                        }
-                    }
-                    Events::SendKeepalive(_) => {
-                        info!("Sending keepalive");
-                        connection_manager.greet_all_endpoints().await;
-
-                        // Reopen dead connections
-                        connection_manager.ensure_endpoint_connections(server_endpoint_id, &connection_info).await;
-                        if let Some(interface_logger) = &mut interface_logger {
-                            interface_logger.flush().await;
-                        }
-
-                        if let Some(packet_sorter_logger) = &mut packet_sorter_logger {
-                            packet_sorter_logger.flush().await;
-                        }
+            match wrapped_keepalive_timer
+                .or(
+                    wrapped_connection_timeout
+                    .race(wrapped_packet_sorter)
+                    .race(wrapped_tunnel_device)
+                    .race(wrapped_endpoints)
+                    .race(wrapped_new_sorted_packet)
+                    .race(wrapped_duplication_socket)
+                )
+                .await
+            {
+                Events::NewEstablishedMessage(result) => match result {
+                    Ok(read_info) => {
+                        //info!("Endpoint: {}, produced message: {:?}", endpointid, message);
+                        connection_manager.handle_established_message(
+                            read_info,
+                            &mut interface_logger).await;
 
                     }
-                    Events::NewSortedPacket((_endpoint_id, maybe_packet)) => {
-                        if let Some(packet) = maybe_packet {
-                            if let Some(packet_sorter_logger) = &mut packet_sorter_logger {
-                                packet_sorter_logger.add_log_line(
-                                    packet.seq
-                                ).await
-                            }
-
-                            tun.send(packet.bytes.as_slice()).await.unwrap();
-                        }
+                    Err(e) => {
+                        error!("Encountered error: {}", e.to_string())
                     }
-                    Events::DuplicationMessage((size, _addr)) => {
-                        let duplication_message = &duplication_message_buffer[..size];
-                        debug!("Duplication raw: {:?}", duplication_message);
-                        connection_manager.handle_selective_duplication_command(duplication_message);
+                },
+                Events::ConnectionTimeout((endpoint, interface_name, socket)) => {
+                    connection_manager.remove_connection(endpoint, interface_name, socket)
+                }
+                Events::PacketSorter(endpoint_id) => {
+                    connection_manager.handle_packet_sorter_deadline(endpoint_id).await;
+                }
+                Events::TunnelPacket(maybe_packet) => {
+                    match maybe_packet {
+                        Ok(packet_length) => {
+                            connection_manager.handle_packet_from_tun(&tun_buffer[..packet_length]).await;
+                        }
+                        Err(e) => error!("Error while reading from tun device: {}", e.to_string())
                     }
                 }
-                connection_manager.remove_disconnected();
+                Events::SendKeepalive(_) => {
+                    info!("Sending keepalive");
+                    connection_manager.greet_all_endpoints().await;
 
-            } else {
-                info!("No active connections. Awaiting new connection attempts.");
-
-                for interface_config in &settings.interfaces {
-                    match connection_manager.create_new_connection(
-                        interface_config.interface_name.clone(),
-                        interface_config.bind_address,
-                        interface_config.server_address,
-                        server_endpoint_id
-                    ).await {
-                        Ok(()) => { debug!("Restarted connection on: {}", interface_config.interface_name) }
-                        Err(_) => { error!("Failed to restart connection on: {}", interface_config.interface_name) }
+                    // Reopen dead connections
+                    connection_manager.ensure_endpoint_connections(server_endpoint_id, &connection_info).await;
+                    if let Some(interface_logger) = &mut interface_logger {
+                        interface_logger.flush().await;
                     }
+
+                    if let Some(packet_sorter_logger) = &mut packet_sorter_logger {
+                        packet_sorter_logger.flush().await;
+                    }
+
+                }
+                Events::NewSortedPacket((_endpoint_id, maybe_packet)) => {
+                    if let Some(packet) = maybe_packet {
+                        if let Some(packet_sorter_logger) = &mut packet_sorter_logger {
+                            packet_sorter_logger.add_log_line(
+                                packet.seq
+                            ).await
+                        }
+
+                        tun.send(packet.bytes.as_slice()).await.unwrap();
+                    }
+                }
+                Events::DuplicationMessage((size, _addr)) => {
+                    let duplication_message = &duplication_message_buffer[..size];
+                    debug!("Duplication raw: {:?}", duplication_message);
+                    connection_manager.handle_selective_duplication_command(duplication_message);
+                }
+            }
+            connection_manager.remove_disconnected();
+
+        } else {
+            info!("No active connections. Awaiting new connection attempts.");
+
+            for interface_config in &settings.interfaces {
+                match connection_manager.create_new_connection(
+                    interface_config.interface_name.clone(),
+                    interface_config.bind_address,
+                    interface_config.server_address,
+                    server_endpoint_id
+                ).await {
+                    Ok(()) => { debug!("Restarted connection on: {}", interface_config.interface_name) }
+                    Err(_) => { error!("Failed to restart connection on: {}", interface_config.interface_name) }
                 }
             }
         }
-    });
+    }
 }
