67bd40abbf98eaf0f048ae22c65073b5dcd951f3
drblah <theblahblah2001@gmail.com>
2026-07-10 08:54:57 +0200

    Remove feature gate for io_more because it is now part of stable

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,28 +1,26 @@
-#![feature(io_error_more)]
-
 extern crate core;
 
-use nix::libc;
-use std::io::Error;
+use crate::messages::EndpointId;
 use anyhow::Result;
+use log::error;
+use nix::libc;
+use socket2::{Domain, Socket, Type};
+use std::io::Error;
+use std::net::UdpSocket as std_udp;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4};
 use tokio::net::UdpSocket;
-use socket2::{Domain, Socket, Type};
-use std::net::UdpSocket as std_udp;
-use log::error;
-use crate::messages::EndpointId;
 
-pub mod messages;
-pub mod packet_sorter;
 pub mod connection;
-pub mod endpoint;
 pub mod connection_manager;
-pub mod settings;
+pub mod endpoint;
+pub mod interface_logger;
+pub mod messages;
+mod packet_queue;
+pub mod packet_sorter;
+pub mod packet_sorter_log;
 mod path_latency;
 pub mod router;
-pub mod interface_logger;
-pub mod packet_sorter_log;
-mod packet_queue;
+pub mod settings;
 
 pub fn interface_to_ipaddr(interface: &str) -> Result<Ipv4Addr, Error> {
     let interfaces = pnet_datalink::interfaces();
@@ -37,14 +35,12 @@ pub fn interface_to_ipaddr(interface: &str) -> Result<Ipv4Addr, Error> {
         .find(|ip| ip.is_ipv4())
         .ok_or_else(|| std::io::ErrorKind::AddrNotAvailable)?;
 
-
     if let IpAddr::V4(ipaddr) = ipaddr.ip() {
-        return Ok(ipaddr)
+        return Ok(ipaddr);
     }
 
     Err(Error::from(std::io::ErrorKind::AddrNotAvailable))
 }
-
 
 /// This function makes a tokio UdpSocket which is bound to the specified IP and port
 /// Optionally, it can also bind to a specific network device. Binding ensures this
@@ -52,28 +48,28 @@ pub fn interface_to_ipaddr(interface: &str) -> Result<Ipv4Addr, Error> {
 /// This is useful when multiple interfaces can route to the same destination, but you want
 /// to control which interface is used. *NB*: The bind to device socket option is only supported
 /// on Linux.
-fn make_socket(interface: &str, local_address: Option<Ipv4Addr>, local_port: Option<u16>, bind_to_device: bool) -> Result<UdpSocket> {
+fn make_socket(
+    interface: &str,
+    local_address: Option<Ipv4Addr>,
+    local_port: Option<u16>,
+    bind_to_device: bool,
+) -> Result<UdpSocket> {
     let socket = Socket::new(Domain::IPV4, Type::DGRAM, None).unwrap();
 
     if bind_to_device {
         if let Err(err) = socket.bind_device(Some(interface.as_bytes())) {
             if matches!(err.raw_os_error(), Some(libc::ENODEV)) {
                 error!("error binding to device (`{}`): {}", interface, err);
-                return Err(anyhow::Error::new(err))
+                return Err(anyhow::Error::new(err));
             } else {
                 panic!("unexpected error binding device: {}", err);
             }
         }
     }
 
-
     let local_address = match local_address {
-        Some(local_address) => {
-            local_address
-        }
-        None => {
-            interface_to_ipaddr(interface).unwrap()
-        }
+        Some(local_address) => local_address,
+        None => interface_to_ipaddr(interface).unwrap(),
     };
 
     let address = if local_port.is_some() {
